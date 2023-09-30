19c5466d0f4a8ea98a80fb793a20d881a47b89d1
millerdotkyle <kylenotfound@gmail.com>
2023-09-30 14:24:02 -0400

    changed product delete
    -behaves same as other delete routes now

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -106,13 +106,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
-  const productData = await Product.destroy({
+  Product.destroy({
     where: {
       id: req.params.id,
     },
-  })
-
-  res.json(productData);
+    }).then(()=> {
+      res.status(200).statusMessage('Deleted!')
+    })
 
 });
 
